b1f9dcce20c8b9d96107eeeea814dde101857900
dmitrinesterenko <dmitri.nesterenko@gmail.com>
2014-05-06 22:00:29 -0400

    Using the where clause to define a local function

--- bmiCalc.hs
+++ bmiCalc.hs
@@ -1,11 +1,11 @@
 import System.Environment
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height  
-    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"  
-    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
-    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
     | otherwise                 = "You're a whale, congratulations!"
-
+    where bmi = weight / height  ^ 2
 main = do 
 	[weight,height] <- getArgs
 	putStrLn (bmiTell (read weight :: Float) (read height :: Float))  
